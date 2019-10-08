550a0b9ef7989f1726688fda18b7424d9abe5ce6
Wending Lu <wlu@gmail.com>
2019-10-08 15:19:46 -0700

    responsive and favicons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+  //Obfuscate email
+  $(".contact").click(function() {
+    const username = "i.am.wending";
+    const domain = "gmail.com";
 
+    window.location.href =
+      "mailto:" +
+      username.substring(0) +
+      "@" +
+      domain.substring(0);
+
+    console.log(username);
+  });
 });
